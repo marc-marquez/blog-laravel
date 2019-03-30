33158aff7d3d00b010f8ca7b0bbf740c660421f4
Marc Marquez <marc.marquez@gmail.com>
2019-03-29 22:09:49 -0700

    Added update and destroy functionality. Added delete button to show form.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" type="button" class="btn btn-default">Go Back</a>
+    <a href="/posts" role="button" class="btn btn-secondary">Go Back</a>
     <h1>{{$post->title}}</h1>
     <div>
         {!!$post->body!!}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" role="button" class="btn btn-primary">Edit</a>
+
+    {!!Form::open(['action' => ['PostsController@destroy',$post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method','DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
